62b527d9d441d745187793a53a4ea3b8f94ec4d3
Yao-Xinchen <yao29@illinois.edu>
2023-09-20 17:49:21 +0800

    omni_chassis complete

--- decomposition/omni_chassis/include/omni_chassis/omni_kinematics.hpp
+++ decomposition/omni_chassis/include/omni_chassis/omni_kinematics.hpp
@@ -7,24 +7,18 @@
 #include "movement_interface/msg/natural_move.hpp"
 #include "movement_interface/msg/absolute_move.hpp"
 #include "motor_interface/msg/motor_goal.hpp"
-#include "gyro_interface/srv/chassis_position.hpp"
 #include "gyro_interface/srv/gimbal_position.hpp"
+#include "motor_interface/msg/motor_present.hpp"
 
 #define PI 3.14159265358979323846
 #define RADIUS 0.1 // meter
-#define LF0 0
-#define RF0 1
-#define LB0 2
-#define RB0 3
-#define LF1 4
-#define RF1 5
-#define LB1 6
-#define RB1 7
+#define F 0
+#define L 1
+#define B 2
+#define R 3
 
 namespace OmniKinematics
 {
-    motor_interface::msg::MotorGoal natural_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg,
-                                                    float chassis_yaw, float gimbal_yaw);
     motor_interface::msg::MotorGoal absolute_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg,
                                                      float chassis_yaw);
 
@@ -32,8 +26,6 @@ namespace OmniKinematics
                   const int motor_id, const float goal_vel, const float goal_pos);
 
     void clear_goals(motor_interface::msg::MotorGoal &motor_goals);
-
-    float rss(float x, float y); // root sum square
 };
 
 #endif // OMNI_KINEMATICS_HPP
--- decomposition/omni_chassis/src/omni_kinematics.cpp
+++ decomposition/omni_chassis/src/omni_kinematics.cpp
@@ -1,15 +1,15 @@
 #include "omni_chassis/omni_kinematics.hpp"
 
-motor_interface::msg::MotorGoal OmniKinematics::natural_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float chassis_yaw, float gimbal_yaw)
-{
-    motor_interface::msg::MotorGoal motor_goals;
-
-    return motor_goals;
-}
-
 motor_interface::msg::MotorGoal OmniKinematics::absolute_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw)
 {
     motor_interface::msg::MotorGoal motor_goals;
+    float rot = msg->omega * RADIUS;
+
+    clear_goals(motor_goals);
+    add_goal(motor_goals, F, msg->vel_x * cos(chassis_yaw) - msg->vel_y * sin(chassis_yaw) + rot, 0);
+    add_goal(motor_goals, L, - msg->vel_x * sin(chassis_yaw) - msg->vel_y * cos(chassis_yaw) + rot, 0);
+    add_goal(motor_goals, B, - msg->vel_x * cos(chassis_yaw) + msg->vel_y * sin(chassis_yaw) + rot, 0);
+    add_goal(motor_goals, R, msg->vel_x * sin(chassis_yaw) + msg->vel_y * cos(chassis_yaw) + rot, 0);
 
     return motor_goals;
 }
@@ -26,9 +26,4 @@ void OmniKinematics::clear_goals(motor_interface::msg::MotorGoal &motor_goals)
     motor_goals.motor_id.clear();
     motor_goals.goal_pos.clear();
     motor_goals.goal_vel.clear();
-}
-
-float OmniKinematics::rss(float x, float y)
-{
-    return std::sqrt(x * x + y * y);
 }
