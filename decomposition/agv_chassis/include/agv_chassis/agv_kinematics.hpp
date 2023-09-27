e6c5ea65386f1a596d7fd07d3d0b7a87305c2ac1
Yao-Xinchen <yao29@illinois.edu>
2023-09-27 20:33:16 +0800

    agv algorithm complete

--- decomposition/agv_chassis/include/agv_chassis/agv_kinematics.hpp
+++ decomposition/agv_chassis/include/agv_chassis/agv_kinematics.hpp
@@ -2,14 +2,15 @@
 #define AGV_KINEMATICS_HPP
 
 #include <cmath>
+#include <motor_interface/msg/detail/dm_goal__struct.hpp>
 #include <vector>
 
 #include "movement_interface/msg/natural_move.hpp"
 #include "movement_interface/msg/absolute_move.hpp"
-#include "motor_interface/msg/motor_goal.hpp"
+#include "motor_interface/msg/dm_goal.hpp"
 #include "motor_interface/msg/dji_goal.hpp"
 #include "gyro_interface/srv/gimbal_position.hpp"
-#include "motor_interface/msg/motor_present.hpp"
+#include "motor_interface/srv/motor_present.hpp"
 
 #define PI 3.14159265358979323846
 #define RADIUS 0.1 // meter
@@ -32,17 +33,19 @@ enum VelocityMotor
 
 namespace AgvKinematics
 {
-    motor_interface::msg::MotorGoal natural_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg,
-                                                    float yaw_diff);
-    motor_interface::msg::MotorGoal absolute_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg,
-                                                     float chassis_yaw);
+    motor_interface::msg::DmGoal natural_dm_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float yaw_diff);
 
-    void add_goal(motor_interface::msg::MotorGoal &motor_goals,
-                  const int motor_id, const float goal_vel, const float goal_pos);
+    motor_interface::msg::DmGoal absolute_dm_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw);
+
+    motor_interface::msg::DjiGoal natural_dji_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float yaw_diff);
+
+    motor_interface::msg::DjiGoal absolute_dji_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw);
+
+    void add_dm_goal(motor_interface::msg::DmGoal &motor_goals, const DirectionMotor motor_id, const float goal_vel, const float goal_pos);
 
     void set_dji_goal(motor_interface::msg::DjiGoal &motor_goals, const VelocityMotor motor_id, const float goal_pos);
 
-    void clear_goals(motor_interface::msg::MotorGoal &motor_goals);
+    void clear_dm_goals(motor_interface::msg::DmGoal &motor_goals);
 
     float rss(float x, float y); // root sum square
 };
--- decomposition/agv_chassis/src/agv_kinematics.cpp
+++ decomposition/agv_chassis/src/agv_kinematics.cpp
@@ -2,54 +2,76 @@
 #include <cmath>
 #include <math.h>
 
-motor_interface::msg::MotorGoal AgvKinematics::natural_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float yaw_diff)
+motor_interface::msg::DmGoal AgvKinematics::natural_dm_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float yaw_diff)
 {
-    motor_interface::msg::MotorGoal motor_goals;
+    motor_interface::msg::DmGoal dm_goal;
     float trans_n = msg->vel_tau * sin(yaw_diff) + msg->vel_n * cos(yaw_diff);
     float trans_tao = msg->vel_tau * cos(yaw_diff) - msg->vel_n * sin(yaw_diff);
     float rot_vel = msg->omega * RADIUS / sqrt(2);
 
-    clear_goals(motor_goals);
-    add_goal(motor_goals, LF_V, rss(trans_n + rot_vel, trans_tao - rot_vel), 0);
-    add_goal(motor_goals, RF_V, rss(trans_n + rot_vel, trans_tao + rot_vel), 0);
-    add_goal(motor_goals, LB_V, rss(trans_n - rot_vel, trans_tao - rot_vel), 0);
-    add_goal(motor_goals, RB_V, rss(trans_n - rot_vel, trans_tao + rot_vel), 0);
-    add_goal(motor_goals, LF_D, 0, atan((trans_n + rot_vel) / (trans_tao - rot_vel)));
-    add_goal(motor_goals, RF_D, 0, atan((trans_n + rot_vel) / (trans_tao + rot_vel)));
-    add_goal(motor_goals, LB_D, 0, atan((trans_n - rot_vel) / (trans_tao - rot_vel)));
-    add_goal(motor_goals, RB_D, 0, atan((trans_n - rot_vel) / (trans_tao + rot_vel)));
+    clear_dm_goals(dm_goal);
+    add_dm_goal(dm_goal, LF_D, atan((trans_n + rot_vel) / (trans_tao - rot_vel)), 0);
+    add_dm_goal(dm_goal, RF_D, atan((trans_n + rot_vel) / (trans_tao + rot_vel)), 0);
+    add_dm_goal(dm_goal, LB_D, atan((trans_n - rot_vel) / (trans_tao - rot_vel)), 0);
+    add_dm_goal(dm_goal, RB_D, atan((trans_n - rot_vel) / (trans_tao + rot_vel)), 0);
 
-    return motor_goals;
+    return dm_goal;
 }
 
-motor_interface::msg::MotorGoal AgvKinematics::absolute_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw)
+motor_interface::msg::DjiGoal AgvKinematics::natural_dji_decompo(const movement_interface::msg::NaturalMove::SharedPtr msg, float yaw_diff)
 {
-    motor_interface::msg::MotorGoal motor_goals;
+    motor_interface::msg::DjiGoal dji_goal;
+    float trans_n = msg->vel_tau * sin(yaw_diff) + msg->vel_n * cos(yaw_diff);
+    float trans_tao = msg->vel_tau * cos(yaw_diff) - msg->vel_n * sin(yaw_diff);
+    float rot_vel = msg->omega * RADIUS / sqrt(2);
+
+    set_dji_goal(dji_goal, LF_V, rss(trans_n + rot_vel, trans_tao - rot_vel));
+    set_dji_goal(dji_goal, RF_V, rss(trans_n + rot_vel, trans_tao + rot_vel));
+    set_dji_goal(dji_goal, LB_V, rss(trans_n - rot_vel, trans_tao - rot_vel));
+    set_dji_goal(dji_goal, RB_V, rss(trans_n - rot_vel, trans_tao + rot_vel));
+
+    return dji_goal;
+}
+
+motor_interface::msg::DmGoal AgvKinematics::absolute_dm_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw)
+{
+    motor_interface::msg::DmGoal dm_goal;
     float trans_n = msg->vel_x * cos(chassis_yaw) - msg->vel_y * sin(chassis_yaw);
     float trans_tao = msg->vel_x * sin(chassis_yaw) + msg->vel_y * cos(chassis_yaw);
     float rot_vel = msg->omega * RADIUS / sqrt(2);
 
-    clear_goals(motor_goals);
-    add_goal(motor_goals, LF_V, rss(trans_n + rot_vel, trans_tao - rot_vel), 0);
-    add_goal(motor_goals, RF_V, rss(trans_n + rot_vel, trans_tao + rot_vel), 0);
-    add_goal(motor_goals, LB_V, rss(trans_n - rot_vel, trans_tao - rot_vel), 0);
-    add_goal(motor_goals, RB_V, rss(trans_n - rot_vel, trans_tao + rot_vel), 0);
-    add_goal(motor_goals, LF_D, 0, atan((trans_n + rot_vel) / (trans_tao - rot_vel)));
-    add_goal(motor_goals, RF_D, 0, atan((trans_n + rot_vel) / (trans_tao + rot_vel)));
-    add_goal(motor_goals, LB_D, 0, atan((trans_n - rot_vel) / (trans_tao - rot_vel)));
-    add_goal(motor_goals, RB_D, 0, atan((trans_n - rot_vel) / (trans_tao + rot_vel)));
+    clear_dm_goals(dm_goal);
+    add_dm_goal(dm_goal, LF_D, atan((trans_n + rot_vel) / (trans_tao - rot_vel)), 0);
+    add_dm_goal(dm_goal, RF_D, atan((trans_n + rot_vel) / (trans_tao + rot_vel)), 0);
+    add_dm_goal(dm_goal, LB_D, atan((trans_n - rot_vel) / (trans_tao - rot_vel)), 0);
+    add_dm_goal(dm_goal, RB_D, atan((trans_n - rot_vel) / (trans_tao + rot_vel)), 0);
 
-    return motor_goals;
+    return dm_goal;
 }
 
-void AgvKinematics::add_goal(motor_interface::msg::MotorGoal &motor_goals, const int motor_id, const float goal_vel, const float goal_pos)
+motor_interface::msg::DjiGoal AgvKinematics::absolute_dji_decompo(const movement_interface::msg::AbsoluteMove::SharedPtr msg, float chassis_yaw)
+{
+    motor_interface::msg::DjiGoal dji_goal;
+    float trans_n = msg->vel_x * cos(chassis_yaw) - msg->vel_y * sin(chassis_yaw);
+    float trans_tao = msg->vel_x * sin(chassis_yaw) + msg->vel_y * cos(chassis_yaw);
+    float rot_vel = msg->omega * RADIUS / sqrt(2);
+
+    set_dji_goal(dji_goal, LF_V, rss(trans_n + rot_vel, trans_tao - rot_vel));
+    set_dji_goal(dji_goal, RF_V, rss(trans_n + rot_vel, trans_tao + rot_vel));
+    set_dji_goal(dji_goal, LB_V, rss(trans_n - rot_vel, trans_tao - rot_vel));
+    set_dji_goal(dji_goal, RB_V, rss(trans_n - rot_vel, trans_tao + rot_vel));
+
+    return dji_goal;
+}
+
+void AgvKinematics::add_dm_goal(motor_interface::msg::DmGoal &motor_goals, const DirectionMotor motor_id, const float goal_vel, const float goal_pos)
 {
     motor_goals.motor_id.push_back(motor_id);
     motor_goals.goal_vel.push_back(goal_vel);
     motor_goals.goal_pos.push_back(goal_pos);
 }
 
-void AgvKinematics::clear_goals(motor_interface::msg::MotorGoal &motor_goals)
+void AgvKinematics::clear_dm_goals(motor_interface::msg::DmGoal &motor_goals)
 {
     motor_goals.motor_id.clear();
     motor_goals.goal_pos.clear();
