3935706f17388903b6176f9fbbc46a6933a157c6
Yao Xinchen <yao29@illinois.edu>
2023-11-30 17:17:42 +0800

    doc added to dm_driver

--- execution/dm_controller/include/dm_controller/can_driver.hpp
+++ execution/dm_controller/include/dm_controller/can_driver.hpp
@@ -19,6 +19,11 @@ private:
     struct ifreq ifr;
 
 public:
+    /**
+     * @brief Constructor for the CanDriver class.
+     * 
+     * @param port The port number to use for the CAN driver. Default is 0.
+     */
     CanDriver(int port = 0)
     {
         s = socket(PF_CAN, SOCK_RAW, CAN_RAW); // open the CAN socket
@@ -32,17 +37,29 @@ public:
 
         int bind_result = bind(s, (struct sockaddr *)&addr, sizeof(addr)); // bind the socket to the CAN interface
         if (bind_result == -1) perror("Error binding socket to CAN interface");
+        else printf("Socket bound to CAN interface\n");
     }
 
+    /**
+     * @brief Destructor for the CanDriver class.
+     * 
+     * This destructor closes the socket connection.
+     */
     ~CanDriver()
     {
-        // close the CAN socket
         close(s);
     }
-
-    void send_frame(can_frame frame)
+    
+    /**
+     * Sends a CAN frame over the CAN bus.
+     *
+     * @param frame The CAN frame to be sent.
+     */
+    void send_frame(const can_frame &frame)
     {
-        write(s, &frame, sizeof(frame));
+        auto result = write(s, &frame, sizeof(frame));
+        if (result == -1) perror("Error sending CAN frame");
+        else printf("CAN frame sent\n");
     }
 };
 
