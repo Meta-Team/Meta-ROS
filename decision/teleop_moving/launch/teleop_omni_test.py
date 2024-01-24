0c5931f45cf8bc036f2ed575685e06d585847638
Yao-Xinchen <yao29@illinois.edu>
2024-01-24 19:30:58 +0800

    launch added to teleop_moving

---
+++ decision/teleop_moving/launch/teleop_omni_test.py
@@ -0,0 +1,30 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('omni_chassis'),
+        'config',
+        'omni_chassis_test.yaml'
+    )
+    return LaunchDescription([
+        Node(
+            package='omni_chassis',
+            executable='omni_chassis',
+            name='omni_chassis',
+            parameters=[config],
+        ),
+        Node(
+            package='dji_controller',
+            executable='dji_controller',
+            name='dji_controller',
+            parameters=[config],
+        ),
+        Node(
+            package='teleop_moving',
+            executable='teleop_moving',
+            name='teleop_moving',
+        )
+    ])
