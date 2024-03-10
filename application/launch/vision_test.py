ff30ef956884d218d86add7c9b4218333efb9e9d
Yao-Xinchen <yao29@illinois.edu>
2024-03-10 22:04:11 +0800

    combining meta-vision

---
+++ application/launch/vision_test.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, GroupAction,
+                            IncludeLaunchDescription, SetEnvironmentVariable)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    ahrs_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('fdilink_ahrs'),
+                        'launch/ahrs_driver.launch.py')
+        )
+    )
+
+    vision_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('solais_interpreter'),
+                        'launch/vision_bringup.launch.py')
+        )
+    )
+
+    ld = LaunchDescription([
+    ])
+
+    ld.add_action(ahrs_launch)
+    ld.add_action(vision_launch)
+
+    return ld
