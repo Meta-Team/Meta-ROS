ad14c2cb62deda2a32db719dbc868f0ad35066ac
Yao-Xinchen <yao29@illinois.edu>
2024-03-27 12:48:27 +0800

    create ahrs_interpreter

---
+++ perception/ahrs_feedback/ahrs_interpreter/src/ahrs_interpreter.cpp
@@ -0,0 +1,38 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "ahrs_interface/msg/angular_position.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+
+class AhrsInterpreter : public rclcpp::Node
+{
+public:
+    AhrsInterpreter() : Node("ahrs_interpreter")
+    {
+        sub_ = this->create_subscription<geometry_msgs::msg::Vector3>(
+            "euler_angles", 10, [this](const geometry_msgs::msg::Vector3::SharedPtr msg){
+                callback(msg);
+            });
+        pub_ = this->create_publisher<ahrs_interface::msg::AngularPosition>("angular_position", 10);
+    }
+
+private:
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr sub_;
+    rclcpp::Publisher<ahrs_interface::msg::AngularPosition>::SharedPtr pub_;
+
+    void callback(const geometry_msgs::msg::Vector3::SharedPtr feedback_msg)
+    {
+        ahrs_interface::msg::AngularPosition pub_msg;
+        pub_msg.yaw = - feedback_msg->z;
+        pub_msg.pitch = feedback_msg->y;
+        pub_->publish(pub_msg);
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node_ = std::make_shared<AhrsInterpreter>();
+    rclcpp::spin(node_);
+    rclcpp::shutdown();
+    return 0;
+}
