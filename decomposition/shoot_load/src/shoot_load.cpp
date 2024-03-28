17879af6e4c407892e24f8c74acf40edc153124e
Yao-Xinchen <yao29@illinois.edu>
2024-03-28 15:42:46 +0800

    add drawback logic to shoot load

--- decomposition/shoot_load/src/shoot_load.cpp
+++ decomposition/shoot_load/src/shoot_load.cpp
@@ -1,79 +1,158 @@
 #include "rclcpp/rclcpp.hpp"
+#include <chrono>
+#include <cstdint>
+#include <functional>
+#include <memory>
 
 #include "behavior_interface/msg/shoot.hpp"
 #include "motor_interface/msg/motor_goal.hpp"
+#include "motor_interface/msg/motor_state.hpp"
 
 #define NaN std::nan("")
+
+#define PUB_FREQ 10 // ms
 
 class ShootLoad : public rclcpp::Node
 {
 public:
     ShootLoad() : Node("shoot_load")
     {
-        shoot_sub_ = this->create_subscription<behavior_interface::msg::Shoot>("shoot",
-            10, std::bind(&ShootLoad::shoot_callback, this, std::placeholders::_1));
-        motor_pub_ = this->create_publisher<motor_interface::msg::MotorGoal>("motor_goal", 10);
-        vib_timer_ = this->create_wall_timer(std::chrono::milliseconds(10),
-            std::bind(&ShootLoad::vib_timer_callback, this));
         get_param();
+
+        shoot_sub_ = this->create_subscription<behavior_interface::msg::Shoot>("shoot",
+            10, std::bind(&ShootLoad::goal_callback, this, std::placeholders::_1));
+        feeback_sub_ = this->create_subscription<motor_interface::msg::MotorState>("motor_state",
+            10, std::bind(&ShootLoad::feedback_callback, this, std::placeholders::_1));
+        motor_pub_ = this->create_publisher<motor_interface::msg::MotorGoal>("motor_goal", 10);
+        pub_timer_ = this->create_wall_timer(std::chrono::milliseconds(PUB_FREQ),
+            std::bind(&ShootLoad::pub_timer_callback, this)
+        );
+
         RCLCPP_INFO(this->get_logger(), "ShootLoad initialized.");
     }
 
 private:
     rclcpp::Subscription<behavior_interface::msg::Shoot>::SharedPtr shoot_sub_;
+    rclcpp::Subscription<motor_interface::msg::MotorState>::SharedPtr feeback_sub_;
     rclcpp::Publisher<motor_interface::msg::MotorGoal>::SharedPtr motor_pub_;
-    rclcpp::TimerBase::SharedPtr vib_timer_;
+    rclcpp::TimerBase::SharedPtr pub_timer_;
 
-    float feed_vel_l = 5.0, feed_vel_r = 5.0;
-    float fric_vel_u = 300.0, fric_vel_d = 300.0;
+    // ros params, constant after initialization
+    float feed_max_vel_l = 5.0, feed_max_vel_r = 5.0;
+    float fric_max_vel_u = 300.0, fric_max_vel_d = 300.0;
     float feed_ratio = 36.0, fric_ratio = 1.0;
-    float feed_vib_amp = 1.0, feed_vib_freq = 1.0;
-    float feed_vib = 0.0;
+    int64_t drawback_delay; // ms
+    float jammed_threshold = 0.05; // rad/s, without deceleration
+
+    // local vars
+    float feed_goal_vel_l = 0.0, feed_goal_vel_r;
+    float fric_goal_vel_u = 0.0, fric_goal_vel_d = 0.0;
+    double last_fine_moment_l, last_fine_moment_r;
+    float feed_fb_vel_l = 0.0, feed_fb_vel_r = 0.0;
+    bool active_fric = false, active_feed = false;
 
     void get_param()
     {
-        feed_vel_l = this->declare_parameter("gimbal.feed_vel_l", feed_vel_l);
-        feed_vel_r = this->declare_parameter("gimbal.feed_vel_r", feed_vel_r);
-        fric_vel_u = this->declare_parameter("gimbal.fric_vel_u", fric_vel_u);
-        fric_vel_d = this->declare_parameter("gimbal.fric_vel_d", fric_vel_d);
+        feed_max_vel_l = this->declare_parameter("gimbal.feed_vel_l", feed_max_vel_l);
+        feed_max_vel_r = this->declare_parameter("gimbal.feed_vel_r", feed_max_vel_r);
+        fric_max_vel_u = this->declare_parameter("gimbal.fric_vel_u", fric_max_vel_u);
+        fric_max_vel_d = this->declare_parameter("gimbal.fric_vel_d", fric_max_vel_d);
         feed_ratio = this->declare_parameter("gimbal.feed_ratio", feed_ratio);
         fric_ratio = this->declare_parameter("gimbal.fric_ratio", fric_ratio);
-        RCLCPP_INFO(this->get_logger(), "Feed velocity: %f and %f, Friction velocity: %f and %f", feed_vel_l, feed_vel_r, fric_vel_u, fric_vel_d);
-        feed_vib_freq = this->declare_parameter("gimbal.feed_vib_freq", feed_vib_freq);
-        feed_vib_amp = this->declare_parameter("gimbal.feed_vib_amp", feed_vib_amp);
-        RCLCPP_INFO(this->get_logger(), "Feed vibration frequency: %f and amplitude: %f", feed_vib_freq, feed_vib_amp);
+        drawback_delay = this->declare_parameter("gimbal.drawback_delay", 0x1000000); // no draw back by default
+        jammed_threshold = this->declare_parameter("gimbal.jammed_threshold", 0.05);
+        RCLCPP_INFO(this->get_logger(), "Feed velocity: %f and %f, Friction velocity: %f and %f", feed_max_vel_l, feed_max_vel_r, fric_max_vel_u, fric_max_vel_d);
     }
 
-    void shoot_callback(const behavior_interface::msg::Shoot::SharedPtr msg)
+    void goal_callback(const behavior_interface::msg::Shoot::SharedPtr msg)
     {
-        motor_interface::msg::MotorGoal goal_msg{};
+        active_feed = msg->feed_state;
+        active_fric = msg->fric_state;
+    }
 
-        goal_msg.motor_id.push_back("FEED_L");
-        if (msg->feed_state == true) goal_msg.goal_vel.push_back((feed_vel_l + feed_vib) * feed_ratio);
-        else goal_msg.goal_vel.push_back(feed_vib * feed_ratio);
-        goal_msg.goal_pos.push_back(NaN);
+    void feedback_callback(motor_interface::msg::MotorState::SharedPtr feedback)
+    {
+        for (auto i = 0; i < static_cast<int>(feedback->motor_id.size()); i++)
+        {
+            if (feedback->motor_id[i] == "FEED_L") feed_fb_vel_l = feedback->present_vel[i];
+            if (feedback->motor_id[i] == "FEED_R") feed_fb_vel_r = feedback->present_vel[i];
+        }
+    }
 
-        goal_msg.motor_id.push_back("FEED_R");
-        if (msg->feed_state == true) goal_msg.goal_vel.push_back((feed_vel_r + feed_vib) * feed_ratio);
-        else goal_msg.goal_vel.push_back(feed_vib * feed_ratio);
-        goal_msg.goal_pos.push_back(NaN);
+    void update_timer_callback()
+    {
+        // feed
+        if (active_feed == true) // to shoot
+        {
+            // left
+            if (abs(feed_fb_vel_l) < jammed_threshold) // jammed
+            {
+                // reverse rotation if jammed for long enough
+                auto jammed_duration = rclcpp::Clock().now().seconds() - last_fine_moment_l;
+                if (jammed_duration * 1000 > drawback_delay) feed_goal_vel_l = - feed_max_vel_l;
+            }
+            else // not jammed
+            {
+                last_fine_moment_l = rclcpp::Clock().now().seconds();
+                feed_goal_vel_l = feed_max_vel_l;
+            }
 
-        goal_msg.motor_id.push_back("FRIC_U");
-        if (msg->fric_state == true) goal_msg.goal_vel.push_back(fric_vel_u * fric_ratio);
-        else goal_msg.goal_vel.push_back(0);
-        goal_msg.goal_pos.push_back(NaN);
+            // right
+            if (abs(feed_fb_vel_r) < jammed_threshold) // jammed
+            {
+                // reverse rotation if jammed for long enough
+                auto jammed_duration = rclcpp::Clock().now().seconds() - last_fine_moment_r;
+                if (jammed_duration * 1000 > drawback_delay) feed_goal_vel_r = - feed_max_vel_r;
+            }
+            else // not jammed
+            {
+                last_fine_moment_r = rclcpp::Clock().now().seconds();
+                feed_goal_vel_r = feed_max_vel_r;
+            }
+        }
+        else // not to shoot
+        {
+            feed_goal_vel_l = false;
+            feed_goal_vel_r = false;
+        }
 
-        goal_msg.motor_id.push_back("FRIC_D");
-        if (msg->fric_state == true) goal_msg.goal_vel.push_back(fric_vel_d * fric_ratio);
-        else goal_msg.goal_vel.push_back(0);
-        goal_msg.goal_pos.push_back(NaN);
+        // fric
+        if (active_fric == true)
+        {
+            fric_goal_vel_d = fric_max_vel_d;
+            fric_goal_vel_u = fric_max_vel_u;
+        }
+    }
+
+    void pub_timer_callback()
+    {
+        motor_interface::msg::MotorGoal motor_msg;
+        clear(motor_msg);
+
+        motor_msg.motor_id.push_back("FRIC_U");
+        motor_msg.goal_vel.push_back(fric_goal_vel_u);
+        motor_msg.goal_pos.push_back(NaN);
+
+        motor_msg.motor_id.push_back("FRIC_D");
+        motor_msg.goal_vel.push_back(fric_goal_vel_d);
+        motor_msg.goal_pos.push_back(NaN);
+
+        motor_msg.motor_id.push_back("FEED_L");
+        motor_msg.goal_vel.push_back(feed_goal_vel_l);
+        motor_msg.goal_pos.push_back(NaN);
+
+        motor_msg.motor_id.push_back("FEED_R");
+        motor_msg.goal_vel.push_back(feed_goal_vel_r);
+        motor_msg.goal_pos.push_back(NaN);
         
-        motor_pub_->publish(goal_msg);
+        motor_pub_->publish(motor_msg);
     }
 
-    void vib_timer_callback()
+    void clear(motor_interface::msg::MotorGoal msg)
     {
-        feed_vib = feed_vib_amp * sin(2 * M_PI * feed_vib_freq * rclcpp::Clock().now().seconds());
+        msg.motor_id.clear();
+        msg.goal_vel.clear();
+        msg.goal_pos.clear();
     }
 };
 
