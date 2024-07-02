69c8184e553416cdf3e8b09a18ef447859289bb4
RealMan-Shao <96563821+RealMan-Shao@users.noreply.github.com>
2024-07-02 21:22:26 +0800

    Create robot_state.hpp

---
+++ interfaces/operation_interface/msg/robot_state.hpp
@@ -0,0 +1,19 @@
+uint8_t robot_id; 
+uint8_t robot_level; 
+uint16_t remain_HP; 
+uint16_t max_HP; 
+uint16_t shooter_id1_17mm_cooling_rate; 
+uint16_t shooter_id1_17mm_cooling_limit; 
+uint16_t shooter_id1_17mm_speed_limit; 
+
+uint16_t shooter_id2_17mm_cooling_rate; 
+uint16_t shooter_id2_17mm_cooling_limit; 
+uint16_t shooter_id2_17mm_speed_limit; 
+uint16_t shooter_id1_42mm_cooling_rate; 
+uint16_t shooter_id1_42mm_cooling_limit; 
+uint16_t shooter_id1_42mm_speed_limit; 
+
+uint16_t chassis_power_limit; 
+uint8_t mains_power_gimbal_output : 1; 
+uint8_t mains_power_chassis_output : 1; 
+uint8_t mains_power_shooter_output : 1;
