1a3dfe5824352269d0272264e9e6c2e83f2e64b3
tz61 <mcerztn@gmail.com>
2025-09-04 17:35:20 +0800

    add ch340 mode rules, add can0 setup

--- tools/setupcan.sh
+++ tools/setupcan.sh
@@ -1,6 +1,11 @@
-sudo ip link set can_1 type can bitrate 1000000
-sudo ip link set can_2 type can bitrate 1000000
-sudo ip link set can_3 type can bitrate 1000000
-sudo ip link set can_1 up
-sudo ip link set can_2 up
-sudo ip link set can_3 up
+setup_can () {
+	if [ -d /sys/class/net/$1 ] ; then
+		sudo ip link set $1 type can bitrate 1000000
+		sudo ip link set $1 up
+	fi
+}
+setup_can can_1
+setup_can can_2
+setup_can can_3
+# fallback to some using standalone usb2can devices
+setup_can can0
