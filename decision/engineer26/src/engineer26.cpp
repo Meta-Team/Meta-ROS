4204f25cdbc89968db718d10e1e5cc13e3002927
TonyZhang <mcerztn@gmail.com>
2026-01-15 08:21:24 +0800

    fix conflict

--- decision/engineer26/src/engineer26.cpp
+++ decision/engineer26/src/engineer26.cpp
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#include <functional>
-#include <memory>
-#include <moveit/move_group_interface/move_group_interface.h>
-#include <rclcpp/node.hpp>
-#include <rclcpp/publisher.hpp>
-#include <rclcpp/subscription.hpp>
-#include "geometry_msgs/msg/point_stamped.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "operation_interface/msg/dbus_control.hpp"
-
-=======
 #include "rclcpp/rclcpp.hpp"
 #include "engineer26/dbus_interpreter.h"
 #include "geometry_msgs/msg/twist.hpp"
@@ -17,96 +5,10 @@
 #include <memory>
 
 #define PUB_RATE 15 // ms
->>>>>>> af46c7b (decision for engineer26)
 
 class Engineer26 : public rclcpp::Node
 {
 public:
-<<<<<<< HEAD
-    Engineer26(const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : Node("engineer26", options)
-    {
-        enable_moveit = true;
-        initmove = false;
-        dbus_sub_ = this->create_subscription<operation_interface::msg::DbusControl>(
-            "/dbus_control", 10, std::bind(&Engineer26::dbus_callback, this, std::placeholders::_1));
-        goal_pose_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-            "/goal_pose", 10, std::bind(&Engineer26::clicked_point_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "Engineer26 init");
-    }
-
-    void init_moveit()
-    {
-        move_group_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(shared_from_this(), "meta_engineer_arm");
-
-        move_group_->setMaxVelocityScalingFactor(0.1);
-        move_group_->setMaxAccelerationScalingFactor(0.1);
-
-        RCLCPP_INFO(this->get_logger(), "[test_engineer] init moveit sucess") ;
-    }
-
-    bool initmove;
-
-private:
-    rclcpp::Subscription<operation_interface::msg::DbusControl>::SharedPtr dbus_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr move_pub_;
-    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_;
-
-    bool enable_moveit;
-
-    void dbus_callback(const operation_interface::msg::DbusControl::SharedPtr msg)
-    {
-        double ls_x, ls_y, rs_x, rs_y;
-        std::string lsw = msg->lsw, rsw = msg->rsw;
-        bool wheel;
-        if (lsw == "MID")
-            enable_moveit = true;
-        else
-            enable_moveit = false;
-    }
-    void clicked_point_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
-    {
-        if (!enable_moveit)
-            return;
-        if (!initmove)
-            init_moveit(),
-            initmove = true;
-        RCLCPP_INFO(this->get_logger(), "Received point: (%f, %f, %f)", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
-
-        geometry_msgs::msg::Pose target_pose;
-        target_pose.set__position(msg->pose.position);
-        target_pose.set__orientation(msg->pose.orientation);
-        // orientation
-
-        move_group_->setPoseTarget(target_pose);
-        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-        bool success = (move_group_->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
-
-        RCLCPP_INFO(this->get_logger(), "Visualizing plan (pose goal) %s", success ? "" : "FAILED");
-
-        if (success)
-        {
-            RCLCPP_INFO(this->get_logger(), "Success! Executing...");
-            move_group_->execute(my_plan);
-        }
-        else
-            RCLCPP_ERROR(this->get_logger(), "Plan Failed! Check workspace limits or collision.");
-    }
-};
-
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<Engineer26>();
-    rclcpp::executors::MultiThreadedExecutor executor;
-    executor.add_node(node);
-    node->init_moveit(),
-    node->initmove = false;
-    executor.spin();
-    rclcpp::shutdown();
-    return 0;
-}
-=======
     Engineer26(const rclcpp::NodeOptions & options) : Node("engineer26")
     {
         // get param
@@ -167,4 +69,3 @@ private:
 #include "rclcpp_components/register_node_macro.hpp"
 
 RCLCPP_COMPONENTS_REGISTER_NODE(Engineer26)
->>>>>>> af46c7b (decision for engineer26)
